69c5f00d47993f625e79c503f055bff9c8bddd40
Matt Hisamoto <mwhisamoto@gmail.com>
2012-11-07 17:42:32 -0500

    updated databse

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-set :application, "club40"
-set :repository,  "git@github.com:matthisamoto/club40.git"
+set :application, "gina"
+set :repository,  "git@github.com:matthisamoto/gina.git"
 set :default_environment, { 
   'PATH' => "/home/matt/.rvm/rubies/ruby-1.9.2-p180/bin:/home/matt/.rvm/gems/ruby-1.9.2-p180/bin:/home/matt/.rvm/bin:$PATH",
   'RUBY_VERSION' => 'ruby-1.9.2-p180',
@@ -13,11 +13,11 @@ set :branch, 'master'
 set :user, 'matt'
 
 set :use_sudo, false
-set :deploy_to, "/home/matt/sites/club40"
+set :deploy_to, "/home/matt/sites/gina"
 set :deploy_via, :copy
-set :copy_remote_dir, "/home/matt/sites/club40/tmp"
+set :copy_remote_dir, "/home/matt/sites/gina/tmp"
 set :copy_exclude, [".git"]
 
-role :web, 'club40girl.com'                          # Your HTTP server, Apache/etc
-role :app, 'club40girl.com'                          # This may be the same as your `Web` server
-role :db,  'club40girl.com', :primary => true        # This is where Rails migrations will run
+role :web, 'the-gina-kilpatrick-entourage.com'                          # Your HTTP server, Apache/etc
+role :app, 'the-gina-kilpatrick-entourage.com'                          # This may be the same as your `Web` server
+role :db,  'the-gina-kilpatrick-entourage.com', :primary => true        # This is where Rails migrations will run
