2c8676b854a8c37eeb037e6c0781f2e99c66f458
Matt Hisamoto <mwhisamoto@gmail.com>
2012-11-08 23:17:34 -0500

    added label js

--- public/javascripts/gina.js
+++ public/javascripts/gina.js
@@ -1,42 +1,40 @@
-var List = {
+(function(window, $) {
+	
+	jQuery.fn.labelDisappear = function() {
+		return this.each(function(i,elem) {
+			elem = jQuery(elem);
+			elem.bind({
+				focus: function() {
+					$(this).prevAll("label:first").css('display', "none");
+				},
+				blur: function() {
+					if($(this).val().length === 0) $(this).prevAll("label:first").css('display','block');
+				}
+			});
+		});
+	}
+	
+	var List = {
    
-  setup: function() {
+	  setup: function() {
 
-	$('form').die().live('submit', function(e) {
-	  var name  = $.trim($('.name').val())
-	  var wish = $.trim($('.wish').val())
-	  if( name.length > 0 && wish.length > 0 && name.toLowerCase() != "Name".toLowerCase() && wish.toLowerCase() != "Wish".toLowerCase()) {
-	  } else {
-		alert("Please fill out both fields.")
-		return false;
-	  }
-	})
+		$('form').die().live('submit', function(e) {
+		  var name  = $.trim($('#user_name').val())
+		  var wish = $.trim($('#user_wish').val())
+		  if( name.length > 0 && wish.length > 0 && name.toLowerCase() != "Name".toLowerCase() && wish.toLowerCase() != "Wish".toLowerCase()) {
+		  } else {
+			alert("Please fill out both fields.")
+			return false;
+		  }
+		})
 	
-	$("input[type=text]").each( function() {
-	  var elem = $(this)
-	  elem.click( function () {
-		elem.val("");
-		elem.unbind("click");
-	  })
-	  elem.focus( function () {
-		elem.val("");
-		elem.unbind("focus");
-	  })
-	})
-	
-	$("textarea").each( function() {
-	  var elem = $(this)
-	  elem.click( function () {
-		elem.val("");
-		elem.unbind("click");
-	  })
-	  elem.focus( function () {
-		elem.val("");
-		elem.unbind("focus");
-	  })
-	})
 		
-  }
-}
+		$('#user_name, #user_wish').val("");
+		$('#user_name, #user_wish').labelDisappear();
+		
+	  }
+	}
 
-List.setup();
+	List.setup();
+
+})(window, jQuery);
