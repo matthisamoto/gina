b59fd2b5a0fdb6e197d2acbdb29d8ac15e8e9421
Matt Hisamoto <mwhisamoto@gmail.com>
2012-11-08 21:39:31 -0500

    almost done

--- public/javascripts/gina.js
+++ public/javascripts/gina.js
@@ -3,8 +3,8 @@ var List = {
   setup: function() {
 
 	$('form').die().live('submit', function(e) {
-	  var name  = $.trim($('.user_name').val())
-	  var wish = $.trim($('.user_wish').val())
+	  var name  = $.trim($('.name').val())
+	  var wish = $.trim($('.wish').val())
 	  if( name.length > 0 && wish.length > 0 && name.toLowerCase() != "Name".toLowerCase() && wish.toLowerCase() != "Wish".toLowerCase()) {
 	  } else {
 		alert("Please fill out both fields.")
